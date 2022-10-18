3136d4f5714f4f6c43af826cb79db5b7f2a4a0e7
Vicky Cunill <vickycunill@gmail.com>
2022-10-18 18:56:00 -0300

    actualizacion mensaje error api

--- js/api.js
+++ js/api.js
@@ -3,7 +3,7 @@ const myClima = document.querySelector("#clima");
 fetch("https://api.weatherbit.io/v2.0/current?lang=es&city=Bariloche&country=AR&key=c62a6e3c320440f283903d278708d582")
   .then((response) => {
     if (!response.ok) {
-      throw new Error(`HTTP error, status = ${response.status}`);
+      throw new Error('HTTP error, status = ${response.status}');
     }
     return response.json();
   })
@@ -13,13 +13,16 @@ fetch("https://api.weatherbit.io/v2.0/current?lang=es&city=Bariloche&country=AR&
     
     const textClima = document.createElement("p");
     const nameElement = document.createElement("strong");
-    nameElement.textContent = `El clima en ${dat.data[0].city_name}, ${dat.data[0].temp} grados |  ${dat.data[0].weather.description}`;
+    nameElement.textContent = 'El clima en ${dat.data[0].city_name} | ${dat.data[0].temp} grados |  ${dat.data[0].weather.description}';
     textClima.textContent = nameElement.innerHTML
     myClima.appendChild(textClima);
     
   })
   .catch((error) => {
-    const p = document.createElement("p");
-    p.appendChild(document.createTextNode(`Error: ${error.message}`));
-    document.body.insertBefore(p, myClima);
+    const textClima = document.createElement("p");
+    const nameElement = document.createElement("strong");
+    nameElement.textContent = 'El servicio del clima en Bariloche no está disponible en este momento.'
+    textClima.textContent = nameElement.innerHTML;
+    myClima.appendChild(textClima);
+    //document.body.insertBefore(p, myClima);
   });
