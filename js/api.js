76dbc9d294905b661ccf825f8543f53d3e4eb569
Vicky Cunill <vickycunill@gmail.com>
2022-10-22 10:40:46 -0300

    actualzacion

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
-const myClima = document.querySelector("#clima");
-const textClima = document.createElement("p");
-const imgElement = document.createElement("img");
+const miClima = document.querySelector("#clima");
+const imgClima = document.createElement("img");
+
 
 fetch("https://api.weatherbit.io/v2.0/current?lang=es&city=Bariloche&country=AR&key=06f893dddd5c48a0a9855c22218cbc7b")
   .then((response) => {
@@ -10,16 +10,11 @@ fetch("https://api.weatherbit.io/v2.0/current?lang=es&city=Bariloche&country=AR&
     return response.json();
   })
   .then((dat) => {  
-    textClima.innerHTML = `El clima en ${dat.data[0].city_name} es de ${dat.data[0].temp} grados |  ${dat.data[0].weather.description}`;
-    imgElement.src = `img/icons/${dat.data[0].weather.icon}.png`;
-    imgElement.height = "54";
-    imgElement.width = "24px";
-    imgElement.id = "icon-clima";
-    textClima.appendChild(imgElement);
-    myClima.appendChild(textClima);
+    miClima.innerHTML = `El clima en ${dat.data[0].city_name} es de ${dat.data[0].temp} grados |  ${dat.data[0].weather.description}`;
+    imgClima.src = `img/icons/${dat.data[0].weather.icon}.png`;
+    imgClima.id = "icon-clima";
+    miClima.appendChild(imgClima);
   })
   .catch((error) => {
-    const textClima = document.createElement("p");
-    textClima.innerHTML = "El servicio del clima en Bariloche no está disponible en este momento.";
-    myClima.appendChild(textClima);
+    miClima.innerHTML = "El servicio del clima en Bariloche no está disponible en este momento.";
   });
