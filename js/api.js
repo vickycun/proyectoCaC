9adeac0cd9a0d4730e76d4ff43b3089555410acc
Vicky Cunill <vickycunill@gmail.com>
2022-10-22 11:10:13 -0300

    nueva actualizacion api clima

--- js/api.js
+++ js/api.js
@@ -8,14 +8,14 @@ fetch("https://api.weatherbit.io/v2.0/current?lang=es&city=Bariloche&country=AR&
     return response.json();
   })
   .then((dat) => {
-    const textClima = document.createElement("p");
+    //const textClima = document.createElement("p");
     const imgClima = document.createElement("img");
     imgClima.id = "icon-clima"; 
     imgClima.src = `img/icons/${dat.data[0].weather.icon}.png`;
     imgClima.height = "24";
-    textClima.innerHTML = `El clima en ${dat.data[0].city_name} es de ${dat.data[0].temp} grados |  ${dat.data[0].weather.description}`;
-    textClima.appendChild(imgClima);
-    miClima.appendChild(textClima);
+    miClima.innerHTML = `El clima en ${dat.data[0].city_name} es de ${dat.data[0].temp} grados |  ${dat.data[0].weather.description}`;
+    //textClima.appendChild(imgClima);
+    miClima.appendChild(imgClima);
   })
   .catch((error) => {
     miClima.innerHTML = "El servicio del clima en Bariloche no está disponible en este momento.";
